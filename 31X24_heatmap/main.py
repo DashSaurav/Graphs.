8468c20a4f843274157f287d9b6dc46d477aac5c
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2022-05-03 10:21:42 +0530

    Update main.py

--- 31X24_heatmap/main.py
+++ 31X24_heatmap/main.py
@@ -1,13 +1,20 @@
 import streamlit as st
 from PIL import Image
 import create_heatmap, h_h_map
+import datetime
 st.set_page_config(page_title="Heatmap", layout="wide")
 
 st.subheader("Roomwise Hourly Non-Compliances.")
-#heat_map_data= read_data.heat_map_func(str(currenttime.date()),saved_result,saved_result1)
-heat_map_data= create_heatmap.heat_map_func("2022-04-20",3,4)
-# heat_map_data = {"M1":[0,0,0,0,0,0,3,8,9,9,9,15,15,13,7,16,5,5,0,0,0,0,0,0],
-#                 "M2":[0,0,0,0,0,3,5,8,7,10,10,15,15,14,19,2,2,2,0,0,0,0,0,0]}
+c = st.columns(3)
+with c[0]:
+    val1 = st.number_input("Mention a threshold Value for Room M1", value=2)
+with c[1]:
+    val2 = st.number_input("Mention a threshold Value for Room M2", value=2)
+with c[2]:
+    data_value = st.date_input("Mention a Date",datetime.date(2022, 4, 18))
+
+heat_map_data= create_heatmap.heat_map_func(data_value,int(val1),int(val2))
+# heat_map_data= create_heatmap.heat_map_func("2022-04-20",3,4)
 save_path = "dynamic_images"
 image = h_h_map.create_dashboard_occ_heatmap(heat_map_data,save_path,cmap='Reds')
 st.write(image)
